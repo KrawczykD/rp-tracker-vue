0783ccc5b68edad692809597058a7309e499852d
KrawczykD <40856421+KrawczykD@users.noreply.github.com>
2021-09-22 14:10:56 +0100

    create fetch function for get postcodes

---
+++ src/components/fetchPostcodes.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  getPostcode: function (latLng) {
+    const query =
+      "https://api.mapbox.com/geocoding/v5/mapbox.places/" +
+      latLng.lng +
+      "," +
+      latLng.lat +
+      `.json?access_token=${window.accessToken}`;
+
+    `https://api.mapbox.com/geocoding/v5/mapbox.places/-2.1929475,53.5020051.json?access_token=${window.accessToken}`;
+
+    fetch(query).then((response) => {
+      response.json().then((result) => {
+        return result;
+        // console.log(result.features);
+      });
+    });
+
+    return fetch(query);
+  },
+};
+</script>
